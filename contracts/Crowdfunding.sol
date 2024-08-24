dd54e957e4c74b038238aa39e04d3d2a4cb9ce27
deelight <akingbenidavidoyin@gmail.com>
2024-08-24 19:29:27 +0100

    todo

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -15,11 +15,21 @@ contract CrowdFunding {
   uint goal;
   uint amountRaised;
   uint deadline;
-  constructor(string memory _title, string memory _desc, address _benefactor, uint _goal, uint _duration) {
+  uint id;
+  constructor(uint _id, string memory _title, string memory _desc, address _benefactor, uint _goal, uint _duration) {
     title = _title;
     description = _desc;
     benefactor = _benefactor;
     goal = _goal;
+    id = _id;
     deadline = block.timestamp + _duration;
   }
+
+  ///@notice dontate to a given campaign.
+  function donateToCampaign() {
+    ;
+  }
 }
+
+//TODO: create a factory that will create a new CrowdFunding contract ad save it.
+//And also retrieve a given contract based on its id, using mapping.
