d303dbb278e6e139b5570de9a7780ae55959050c
deelight <akingbenidavidoyin@gmail.com>
2024-08-25 04:46:44 +0100

    Change structure of code and complete tasks

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -9,25 +9,76 @@ pragma solidity ^0.8.24;
 
 contract CrowdFunding {
   /// constructor function
-  string title;
-  string description;
-  address benefactor;
-  uint goal;
-  uint amountRaised;
-  uint deadline;
-  uint id;
-  constructor(uint _id, string memory _title, string memory _desc, address _benefactor, uint _goal, uint _duration) {
-    title = _title;
-    description = _desc;
-    benefactor = _benefactor;
-    goal = _goal;
-    id = _id;
-    deadline = block.timestamp + _duration;
+  
+  struct Campaign{
+    string title;
+    string description;
+    address benefactor;
+    uint goal;
+    uint amountRaised;
+    uint deadline;
+    uint id;
+    bool claimed;
+  }
+  uint private countId = 1;
+
+  // Map id to campaigns.
+  mapping(uint => Campaign) private campaignMapping;
+
+  // Campaign array for storing all campaigns
+  //Campaign[] private campaigns;
+
+  event donateEvent(address sender, uint amount);
+  event createCampaignEvent(uint _id);
+  event endCampaignEvent(bytes _data);
+
+  function createCampaign (string memory _title, string memory _desc, address _benefactor, uint _goal, uint _duration) public {
+    Campaign memory newCampaign = Campaign({
+      title: _title,
+      description: _desc,
+      benefactor: _benefactor,
+      goal: _goal,
+      deadline: block.timestamp + _duration,
+      amountRaised: 0,
+      id: countId,
+      claimed: false
+    });
+    //campaigns.push(newCampaign);
+    campaignMapping[countId] = newCampaign;
+    // Increase count for next contract.
+    emit createCampaignEvent(countId);
+    countId++;
   }
 
+
   ///@notice dontate to a given campaign.
-  function donateToCampaign() {
-    ;
+  function donateToCampaign(uint _campaignId) public payable {
+    // ensure that the campaign_id exists.
+    require(campaignMapping[_campaignId].id > 0, "Campaign ID does not exist");
+    require(block.timestamp < campaignMapping[_campaignId].deadline, "The time for this contract is passed.\nNo longer accepting donations");
+    require(campaignMapping[_campaignId].claimed == false, "This campaign has been ended already.");
+    require(msg.value > 0, "Invalid amount.\n Specify something above 0");
+    campaignMapping[_campaignId].amountRaised += msg.value;
+    emit donateEvent(msg.sender, msg.value);
+  }
+
+  // Define receive function.
+  //receive () payable external {
+  //  emit donateEvent(msg.sender, msg.value);
+  //}
+
+  ///@notice transfer funds to benefactor.
+  function endContract(uint _campaignId) public payable {
+    // ensure that the campaign_id exists.
+    require(campaignMapping[_campaignId].id > 0, "Campaign ID does not exist.");
+    require(block.timestamp >= campaignMapping[_campaignId].deadline, "The contract has not yet reached the specified end date.");
+    require(campaignMapping[_campaignId].claimed == false, "This campaign has been claimed already.");
+    (bool sent, bytes memory data) = (
+      payable(campaignMapping[_campaignId].benefactor)
+    ).call{value: uint128(campaignMapping[_campaignId].amountRaised)}("");
+    require(sent, "Transfer of Funds to benefactor, Unsunccessful. Try again.");
+    if (sent == true) { campaignMapping[_campaignId].claimed = true; }
+    emit endCampaignEvent(data);
   }
 }
 
